e23825ca9dce623750cd48ac4dce342c1e711e3e
Brendan McLaughlin <btmclaughlin03@gmail.com>
2024-09-06 20:18:28 -0400

    support for user input from command line added, prints to console and to exposed endpoint

--- dist/app.js
+++ dist/app.js
@@ -4,12 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const commander_1 = require("commander");
 const app = (0, express_1.default)();
 const port = 3000;
+// Get command line args
+const program = new commander_1.Command();
+program.option('-n, --name <type>', 'name');
+program.parse(process.argv);
+const options = program.opts();
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send(`Hello World! Name: ${options.name}`);
 });
 app.listen(port, () => {
+    console.log(`Name: ${options.name}`);
     return console.log(`Express is listening at http://localhost:${port}`);
 });
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import { Command } from 'commander';
+
 const app = express();
 const port = 3000;
 
+// Get command line args
+const program = new Command();
+program.option('-n, --name <type>', 'name');
+program.parse(process.argv);
+const options = program.opts();
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`Hello World! Name: ${options.name}`);
 });
 
 app.listen(port, () => {
+  console.log(`Name: ${options.name}`);
   return console.log(`Express is listening at http://localhost:${port}`);
 });
 
