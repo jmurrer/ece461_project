1989a5231c0334e756d6b4e133f7751cec7fc31f
Brendan McLaughlin <btmclaughlin03@gmail.com>
2024-09-13 15:47:09 -0400

    updated pipeline to run lint

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,17 @@
-// import express from 'express';       //commented for linter fixing
-import * as readline from 'readline';
+// import * as readline from 'readline';
 
-// const app = express();        //commented for linter fixing 
-// const port = 3000;            //commented for linter fixing
-
-// Initialize CLI
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-// Read CLI
-// let userInput: string = '';        //commented for linter fixing
-rl.on('line', (input: string) => {
-  console.log(`Input: ${input}`);
-// userInput = input;               //commented for linter fixing
-});
-
-// Send data to port
-// app.get('/', (req, res) => {
-//   // res.send('Hello World!');
-//   res.send(`Input: ${userInput}`);
+// // Initialize CLI
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
 // });
 
-// app.listen(port, () => {
-//   //return console.log(`Express is listening at http://localhost:${port}`);
-
+// // Read CLI
+// rl.on('line', (input: string) => {
+//   console.log(`Input: ${input}`);
 // });
 
-// CLose CLI
-rl.on('close', () =>{
-  console.log('Input stream closed...');
-})
+// // CLose CLI
+// rl.on('close', () =>{
+//   console.log('Input stream closed...');
+// });
